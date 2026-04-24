ae8633fd063d926d6c52993d391405d0fa35573c
7quin7 <stroosmaquinten@gmail.com>
2026-04-24 22:03:13 +0200

    Update server.js

--- brainrot-flips-map/server.js
+++ brainrot-flips-map/server.js
@@ -3,8 +3,31 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Website werkt!");
+// 👇 dit is BELANGRIJK
+app.use(express.static("public"));
+
+// coinflip API
+let balance = 100;
+
+app.get("/flip/:bet", (req, res) => {
+  const bet = Number(req.params.bet);
+
+  if (bet > balance) {
+    return res.json({ error: "Not enough balance" });
+  }
+
+  const win = Math.random() < 0.5;
+
+  if (win) {
+    balance += bet;
+  } else {
+    balance -= bet;
+  }
+
+  res.json({
+    result: win ? "WIN" : "LOSE",
+    balance
+  });
 });
 
 app.listen(PORT, () => {
